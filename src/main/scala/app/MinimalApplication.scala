afec6a6e22adaf975610fa290c91a61588e2d467
Teerialho Veikko <vteerialho@gmail.com>
2022-08-29 12:09:03 +0300

    a

--- src/main/scala/app/MinimalApplication.scala
+++ src/main/scala/app/MinimalApplication.scala
@@ -1,19 +1,24 @@
 package app
 import org.w3c.dom.html.HTMLOptGroupElement
-import scalatags.Text.all.{input, _}
 
+import java.nio.charset.Charset
 import java.util.Calendar
+import scala.reflect.internal.NoPhase.id
+
+
+
 
 object MinimalApplication extends cask.MainRoutes{
-// can be found at https://github.com/teeriav1/server
 
-  class Tilaus(val order_nro: Int, val customer: String,val food: String){
 
-      override def toString: String ="#"+order_nro.toString +" to "+ customer+": "+ food + " at "+ time_promised
+  class Tilaus(val order_nro: Int, val customer: String,val food: String,val maksutapa: String){
 
+      override def toString: String = "#"+order_nro.toString +" to "+ customer+": "+ food + " at "+ time_promised +" paid with "+ maksutapa // THIS IS FULL
+      def customersSTring: String =" to "+ this.order_nro.toString +" at "+ time_promised  // THIS IS CUSTOMER ONLY
 
       // TIME ACCESS / laskee luvattua aikaa
       val tilattu = Calendar.getInstance().getTime
+    var a = true
       var luvattu_hour = tilattu.getHours
       var luvattu_mins = tilattu.getMinutes
       var time_to_wait = 20 * yourNumber
@@ -30,7 +35,12 @@ object MinimalApplication extends cask.MainRoutes{
             if (luvattu_hour == 23 ) {luvattu_hour = 0} else {luvattu_hour += 1 }
           }else {}
         }
+        if( luvattu_mins.toString.length != 1) {
+
         time_promised ="Order nro:"+this.order_nro.toString+" ready at: "+ luvattu_hour.toString+"."+luvattu_mins.toString
+        }else {time_promised ="Order nro:"+this.order_nro.toString+" ready at: "+ luvattu_hour.toString+"."+"0"+luvattu_mins.toString}
+
+
       }
       calculate_time_promised
 
@@ -41,9 +51,15 @@ object MinimalApplication extends cask.MainRoutes{
       def tilaukset_stringinä = {
         var palautusString = ""
         if (tilaus_vector.length > 0){
-        //tilaus_vector.foreach( palautusString +=_.time_promised+"\n") tää palauttaa sellaisen näkymän mikä voi näkyä kaikille
-        tilaus_vector.foreach( palautusString+= _+"\n") // admin näkynä
-        palautusString
+
+
+        messages = {Vector(
+        ("", ""),
+        )}
+        tilaus_vector.zipWithIndex.foreach{ case (x,i) => {
+          // messages = messages :+ ("",tilaus_vector(i).toString)  // admin näkymä
+          messages = messages :+ ("",tilaus_vector(i).order_nro.toString+tilaus_vector(i).time_promised.toString)  // customer näkymä
+        }}
       }else{""}}
 
       // yourNumber calculates the number of next order
@@ -52,30 +68,157 @@ object MinimalApplication extends cask.MainRoutes{
         ("", ""),
         ("", ""),
       )}
-  // Tää roska poistuu kun lakkaa auttamasta debuggia
-  def messageList() = {
-    frag(
-      for((customer, food) <- messages)
-      yield p(b(customer), " ", food)
+
+
+  def imgBox(source: String) = div(
+    img(src:=source),
+  )
+  def embed = {
+    div(
+    iframe( width:="560", height:="315", src:="https://www.youtube.com/embed/dQw4w9WgXcQ"),
     )
   }
-// Tää roska poistuu kun lakkaa auttamasta debuggia / end
+
+
+  def messageList() = {
+    meta(charset:="UTF-8")
+
+    var palautusVector: List[String] = List("")
+    frag(
+      for( tilaus <- tilaus_vector )
+      yield p(b(tilaus.toString))
+
+    )
+  }
+    def messageList_Customer() = {
+    meta(charset:="UTF-8")
+
+    var palautusVector: List[String] = List("")
+    frag(
+      for( tilaus <- tilaus_vector )
+      yield p(b(tilaus.customersSTring))
+
+    )
+  }
+
 
   // postHello aka tilauksen käsittely/ formin lähetys
   @cask.postForm("/")
-  def postHello(name: String, msg: String) = {
+  def postHello(name: String, msg: String, maksutapa: String, receipt: String) = { // RECEIPT GIVEN, VALUE NOT USED
     if (name == "") hello(Some("Name cannot be empty"), Some(name), Some(msg))
     else if (name.length >= 20) hello(Some("Name cannot be longer than 20 characters"), Some(name), Some(msg))
     else if (msg == "") hello(Some("Message cannot be empty"), Some(name), Some(msg))
     else if (msg.length >= 160) hello(Some("Message cannot be longer than 160 characters"), Some(name), Some(msg))
     else {
       messages = messages :+ (name -> msg)
-      this.tilaus_vector = this.tilaus_vector :+ new Tilaus(yourNumber, name, msg)
+      this.tilaus_vector = this.tilaus_vector :+ new Tilaus(yourNumber, name, msg, maksutapa)
       hello(None, None, None)
 
     }
   }
-  // "MAIN"
+
+    @cask.postForm("/customer")
+  def postCustomer(name: String, msg: String, maksutapa: String, receipt: String) = { // RECEIPT GIVEN, VALUE NOT USED
+    if (name == "") customer(Some("Name cannot be empty"), Some(name), Some(msg))
+    else if (name.length >= 20) customer(Some("Name cannot be longer than 20 characters"), Some(name), Some(msg))
+    else if (msg == "") customer(Some("Message cannot be empty"), Some(name), Some(msg))
+    else if (msg.length >= 160) customer(Some("Message cannot be longer than 160 characters"), Some(name), Some(msg))
+    else {
+      messages = messages :+ (name -> msg)
+      this.tilaus_vector = this.tilaus_vector :+ new Tilaus(yourNumber, name, msg, maksutapa)
+
+
+      customer(None, None, None)
+
+    }
+  }
+  // "CUSTOMER"
+
+
+  @cask.get("/admin")
+  def customer(errorOpt: Option[String] = None,
+                            userName: Option[String] = None,
+                            msg: Option[String] = None
+                           ) = {
+    html(
+      head(
+        meta(charset:="UTF-8"),
+        link(
+          rel := "stylesheet",
+          href := "https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css",
+
+          src := "https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Atria_Jauhelihapizza.jpg/1280px-Atria_Jauhelihapizza.jpg"
+
+
+        ),
+          scalatags.Text.tags2.title("Pizza orders n sheet Admin view")
+
+      ),
+
+
+      body(
+        meta(charset:="UTF-8"),
+        style := "background-image: url(https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Atria_Jauhelihapizza.jpg/1280px-Atria_Jauhelihapizza.jpg)",
+
+
+        div(cls := "container")(
+          h1("Pizza orders n sheet // Admin view"),
+          h2(s"Please enter your order.\nYour order is number #"+yourNumber.toString),
+          //h3(tilaukset_stringinä),
+          hr,
+          div(id := "Orderlist") (
+            messageList()
+          )),
+
+          hr,
+
+
+          for(error <- errorOpt)
+          yield i(color.red)(error),
+
+
+          // Tässä tulee se pääasiallinen form
+          form(action := "/", method := "post")(
+            meta(charset:="UTF-8"),
+
+
+            input(`type` := "push", placeholder := yourNumber.toString,disabled:="disabled", width := "20%"),
+            input(`type` := "text", name := "name",
+            placeholder := "Name", width := "10%",
+            userName.map( value := _) ),
+            input( `type`:= "text", name := "msg", placeholder := "Please write a your order!", width:="40%", // possible to do Tilauksen jonkinlaisena valikkona
+            msg.map(value := _)),
+            select( id:= "receipt", name:="receipt", // FORM ASKS THIS, DOES NOT CHANGE ANYTHING
+                option("No receipt needed", value:= "No receipt needed"),
+                option("Receipt does not matter", value:= "Receipt does not matter"),
+                option("Receipt wanted, not given", value:= "Receipt wanted, not given"),
+                  ),
+
+            select( id:= "maksutapa", name:= "maksutapa",
+                option("cash", value:="cash"),
+                option("card", value:= "card"),
+                option("other", value:="other"),
+                  ),
+            input(`type` := "submit", width := "10%")
+          ),
+        a(href:="/", "To Customer page"),
+
+      if (tilaus_vector.exists(_.toString.contains("Rick")) ||tilaus_vector.exists(_.toString.contains("rick"))) {embed},
+      )
+
+    )
+  }
+
+
+
+
+
+
+  // "ADMIN"
+
+
+
+
   @cask.get("/")
   def hello(errorOpt: Option[String] = None,
                             userName: Option[String] = None,
@@ -83,38 +226,68 @@ object MinimalApplication extends cask.MainRoutes{
                            ) = {
     html(
       head(
+        meta(charset:="UTF-8"),
         link(
           rel := "stylesheet",
-          href := "https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
-        )
+          href := "https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css",
+
+          src := "https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Atria_Jauhelihapizza.jpg/1280px-Atria_Jauhelihapizza.jpg"
+
+
+        ),
+          scalatags.Text.tags2.title("Pizza orders n sheet")
+
       ),
+
+
       body(
+        meta(charset:="UTF-8"),
+        style := "background-image: url(https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Atria_Jauhelihapizza.jpg/1280px-Atria_Jauhelihapizza.jpg)",
+
+
         div(cls := "container")(
-          h1("Pizza orders n sheet"),
+          h1("Pizza orders n sheet "), // removed Admin View
           h2(s"Please enter your order.\nYour order is number #"+yourNumber.toString),
-          h3(tilaukset_stringinä),
+          //h3(tilaukset_stringinä),
           hr,
           div(id := "Orderlist") (
-            //messageList()
+            messageList_Customer()
           )),
+
           hr,
+
+
           for(error <- errorOpt)
           yield i(color.red)(error),
 
 
           // Tässä tulee se pääasiallinen form
           form(action := "/", method := "post")(
+            meta(charset:="UTF-8"),
+
+
             input(`type` := "push", placeholder := yourNumber.toString,disabled:="disabled", width := "20%"),
             input(`type` := "text", name := "name",
             placeholder := "Name", width := "10%",
             userName.map( value := _) ),
             input( `type`:= "text", name := "msg", placeholder := "Please write a your order!", width:="40%", // possible to do Tilauksen jonkinlaisena valikkona
             msg.map(value := _)),
-            //input(`type`:= select:HTMLOptGroupElement, name := "maksutapa", value := "kortti", value := "kateinen", width:= "20%"), // TODO maksutapa dropdown valikkona
+            select( id:= "receipt", name:="receipt", // FORM ASKS THIS, DOES NOT CHANGE ANYTHING
+                option("No receipt needed", value:= "No receipt needed"),
+                option("Receipt does not matter", value:= "Receipt does not matter"),
+                option("Receipt wanted, not given", value:= "Receipt wanted, not given"),
+                  ),
 
+            select( id:= "maksutapa", name:= "maksutapa",
+                option("cash", value:="cash"),
+                option("card", value:= "card"),
+                option("other", value:="other"),
+                  ),
             input(`type` := "submit", width := "10%")
-          )
+          ),
+        a(href:="/admin", "To Admin page"), // changed from customer
 
+      if (tilaus_vector.exists(_.toString.contains("Rick")) ||tilaus_vector.exists(_.toString.contains("rick"))) {embed},
       )
 
     )
